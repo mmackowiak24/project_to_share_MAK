fae24758764f81c9851dee0a6f90fef80f60dc89
Antoni Krzyżaniak <marvtheminer@gmail.com>
2023-04-24 12:33:50 +0200

    added exercise 4

--- ex_4.py
+++ ex_4.py
@@ -1,4 +1,6 @@
 # Zaokraglij do dwoch miejsc po przecinku
+
 def oblicz_potega(liczba, potega) -> float:
-    # TODO
-    return 0
+    oblicz_potega = pow(liczba, potega)
+    result = round(oblicz_potega, 2)
+    return result
