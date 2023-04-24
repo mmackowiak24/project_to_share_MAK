a15a952f26c90b2abec11df4fc4a4298318fcf25
Mikołaj Maćkowiak <73481@student.ue.poznan.pl>
2023-04-24 13:01:59 +0200

    Fixed ex5

--- ex_5.py
+++ ex_5.py
@@ -1,6 +1,5 @@
 # Oblicz liczbę nawiasów w zmiennej, zwroc: [otwierajace, zamykajace]
-def nawiasy(tekst: str) -> [int, int]:
-    # TODO
+def nawiasy(tekst: str) -> Tuple[int, int]:
     otwierajace = tekst.count("(")
     zamykajace = tekst.count(")")
     return [otwierajace, zamykajace]
